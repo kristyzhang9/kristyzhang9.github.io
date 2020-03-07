bfbe2322b0efc6f56c3750a697864c89068b604c
Kristy Zhang <kristyzhang@Kristys-MBP.attlocal.net>
2020-03-06 20:43:32 -0800

    Worked on homework 6A

---
+++ fluffstuff.js
@@ -0,0 +1,33 @@
+
+function picturechange ()
+{
+	document.getElementById("changepillow").src="purplepillow.jpg" 
+} 
+
+function stuffingclick ()
+{
+	document.getElementById("changestuffing").src="clickedduckdown.jpg" 
+} 
+
+function clickedcouch ()
+{
+	document.getElementById("showclick").src="clickcouch.jpg" 
+} 
+
+function addcart ()
+{
+	document.getElementById("cart").src="addedcart.jpg"
+}
+
+function makenewpillow ()
+{
+	document.getElementById("new").src="makenew.jpg"
+}
+
+
+function gotocart(url) {
+    window.location = url;
+}
+
+
+
